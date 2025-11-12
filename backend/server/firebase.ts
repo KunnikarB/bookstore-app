29f596c4c966aaa40a996b71803f8eb6f1485277
kunnikarB <kunnikarboon@gmail.com>
2025-11-12 13:12:07 +0100

    Update firebase.ts

--- backend/server/firebase.ts
+++ backend/server/firebase.ts
@@ -6,17 +6,25 @@ import dotenv from 'dotenv';
 dotenv.config();
 const require = createRequire(import.meta.url);
 
-// Load service account JSON from path in .env
-const serviceAccountPath = process.env.FIREBASE_KEY_PATH;
+if (!admin.apps.length) {
+  if (process.env.NODE_ENV === 'production') {
+    console.log('⚙️ Using Firebase production credentials');
+    admin.initializeApp({
+      credential: admin.credential.applicationDefault(), // Google Cloud auto credentials
+    });
+  } else {
+    console.log('⚙️ Using Firebase local credentials');
+    const serviceAccountPath = process.env.FIREBASE_KEY_PATH;
 
-if (!serviceAccountPath) {
-  throw new Error('Missing FIREBASE_KEY_PATH in .env');
+    if (!serviceAccountPath) {
+      throw new Error('Missing FIREBASE_KEY_PATH in .env');
+    }
+
+    const serviceAccount = require(serviceAccountPath);
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+  }
 }
-
-const serviceAccount = require(serviceAccountPath);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
 
 export default admin;
