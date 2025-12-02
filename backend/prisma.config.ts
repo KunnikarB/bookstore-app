75cab2806b2916ecf65bd48169ea2b368245f6b8
kunnikarB <kunnikarboon@gmail.com>
2025-12-02 10:39:04 +0100

    add prisma

---
+++ backend/prisma.config.ts
@@ -0,0 +1,10 @@
+import * as Prisma from '@prisma/client';
+const PrismaClient = (Prisma as any).PrismaClient;
+
+export const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL, 
+    },
+  },
+});
